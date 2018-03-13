89b6e6d9f8da8776b316623c8875a38d34992d95
tysta kartografen <tysta.kartografen@gmail.com>
2018-03-13 14:12:31 +0000

    Resolved MySQL db errors.
    institution.Institution.name: (mysql.E001) MySQL does not allow unique CharFields to have a max_length > 255.

--- institution/models.py
+++ institution/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Institution(models.Model):
-    name = models.CharField(max_length=256, unique=True)
+    name = models.CharField(max_length=255, unique=True)
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
 
