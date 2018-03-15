a5225314266d8bc2553bcba9cc9a7e9b8928f172
tysta kartografen <tysta.kartografen@gmail.com>
2018-03-15 14:02:39 +0000

    Added a user profile section.

--- users/admin.py
+++ users/admin.py
@@ -4,10 +4,47 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserChangeForm
 from .forms import CustomUserCreationForm
 from .models import CustomUser
+from .models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    list_display = (
+        'username',
+        'get_account_status',
+        'get_scw_username',
+        'get_shibboleth_username',
+        'is_staff',
+    )
+    list_select_related = ('profile', )
     model = CustomUser
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
+
+    def get_account_status(self, instance):
+        return Profile.STATUS_CHOICES[instance.profile.account_status - 1][1]
+
+    get_account_status.short_description = 'Account Status'
+
+    def get_shibboleth_username(self, instance):
+        return instance.profile.shibboleth_username
+
+    get_shibboleth_username.short_description = 'Shibboleth Username'
+
+    def get_scw_username(self, instance):
+        return instance.profile.scw_username
+
+    get_scw_username.short_description = 'SCW Username'
+
+    def get_inline_instance(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instance(request, obj)
--- users/models.py
+++ users/models.py
@@ -1,17 +1,47 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager
 from django.db import models
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    shibboleth_username = models.CharField(
+        max_length=50,
+        blank=True,
+    )
+    scw_username = models.CharField(
+        max_length=50,
+        blank=True,
+        verbose_name='SCW username',
+    )
+    AWAITING_APPROVAL = 1
+    APPROVED = 2
+    DECLINED = 3
+    REVOKED = 4
+    SUSPENDED = 5
+    CLOSED = 6
+    STATUS_CHOICES = (
+        (AWAITING_APPROVAL, 'Awaiting Approval'),
+        (APPROVED, 'Approved'),
+        (DECLINED, 'Declined'),
+        (REVOKED, 'Revoked'),
+        (SUSPENDED, 'Suspended'),
+        (CLOSED, 'Closed'),
+    )
+    account_status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=AWAITING_APPROVAL)
+
+    class Meta:
+        verbose_name = 'profile'
+        verbose_name_plural = 'profiles'
+        
+    def __str__(self):
+        return self.user.username
 
 class CustomUserManager(UserManager):
     pass
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
-    )
 
     objects = CustomUserManager()
