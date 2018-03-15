2c02d760c183831d59072960beb7ef7a2540f347
tysta kartografen <tysta.kartografen@gmail.com>
2018-03-15 15:28:01 +0000

    Minimal user register form in preparation for shibboleth integration.

--- cogs3/urls.py
+++ cogs3/urls.py
@@ -6,9 +6,25 @@ from django.views.generic.base import TemplateView
 from users.views import Register
 
 urlpatterns = [
-    path('', include('dashboard.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/register/', Register.as_view(), name='register'),
-    path('projects/', include('project.urls')),
-    path('admin/', admin.site.urls),
+    path(
+        '',
+        include('dashboard.urls'),
+    ),
+    path(
+        'accounts/',
+        include('django.contrib.auth.urls'),
+    ),
+    path(
+        'accounts/register/',
+        Register.as_view(),
+        name='register',
+    ),
+    path(
+        'projects/',
+        include('project.urls'),
+    ),
+    path(
+        'admin/',
+        admin.site.urls,
+    ),
 ]
