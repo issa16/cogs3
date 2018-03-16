2a2bc2d3aec1df3deeb5abfa01bdaf153311d86b
tysta kartografen <tysta.kartografen@gmail.com>
2018-03-16 11:32:13 +0000

    Prevent logged in users viewing the login page.

--- cogs3/settings.py
+++ cogs3/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
 
 # Custom user model
--- cogs3/urls.py
+++ cogs3/urls.py
@@ -1,14 +1,21 @@
 """cogs3 URL Configuration"""
 from django.contrib import admin
+from django.contrib import auth
 from django.urls import include
 from django.urls import path
 from django.views.generic.base import TemplateView
+
 from users.views import Register
 
 urlpatterns = [
     path(
         '',
         include('dashboard.urls'),
+    ),
+    path(
+        'accounts/login/',
+        auth.views.LoginView.as_view(redirect_authenticated_user=True),
+        name='login',
     ),
     path(
         'accounts/',
