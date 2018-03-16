641e1abff0c1bcdb8b27f7cf4031f9b65f94eb64
tysta kartografen <tysta.kartografen@gmail.com>
2018-03-16 13:45:39 +0000

    Removed redundant auth check.

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 from project.models import Project
-from project.models import Project
 from project.models import ProjectUserMembership
 
 
@@ -11,12 +10,11 @@ class DashboardView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if self.request.user.is_authenticated:
-            # How many user membership requests are awaiting authorisation?
-            projects = Project.objects.filter(tech_lead=self.request.user)
-            project_user_memberships = ProjectUserMembership.objects.filter(
-                project__in=projects,
-                status=ProjectUserMembership.AWAITING_AUTHORISATION,
-            )
-            context['project_user_requests_count'] = project_user_memberships.count()
+        # How many user membership requests are awaiting authorisation?
+        projects = Project.objects.filter(tech_lead=self.request.user)
+        project_user_memberships = ProjectUserMembership.objects.filter(
+            project__in=projects,
+            status=ProjectUserMembership.AWAITING_AUTHORISATION,
+        )
+        context['project_user_requests_count'] = project_user_memberships.count()
         return context
