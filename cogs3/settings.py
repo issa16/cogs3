bde96fdeda4f9809415c3c23cb69ffb2e780de83
tysta kartografen <tysta.kartografen@gmail.com>
2018-03-16 12:18:43 +0000

    Prevent logged in users viewing the registration page.

--- cogs3/settings.py
+++ cogs3/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
 # Custom user model
--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -8,3 +12,8 @@ class Register(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(reverse('home'))
+        return super().dispatch(*args, **kwargs)
