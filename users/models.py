ac40de887f74643fd6156d7382bd162c41fcd58f
tystakartografen <tystakartografen@gmail.com>
2018-04-11 14:52:01 +0100

    Extended user profile in preparation for data migration.

--- users/models.py
+++ users/models.py
@@ -11,16 +11,45 @@ class Profile(models.Model):
     shibboleth_username = models.CharField(
         max_length=50,
         blank=True,
+        help_text="Institutional address",
     )
     scw_username = models.CharField(
         max_length=50,
         blank=True,
-        verbose_name='SCW username',
+        verbose_name="SCW username",
+    )
+    hpcw_username = models.CharField(
+        max_length=50,
+        blank=True,
+        verbose_name="HPCW username",
+    )
+    hpcw_email = models.EmailField(
+        max_length=100,
+        blank=True,
+        verbose_name="HPCW email address",
+    )
+    raven_username = models.CharField(
+        max_length=50,
+        blank=True,
+        verbose_name="Raven username",
+    )
+    raven_email = models.EmailField(
+        max_length=100,
+        blank=True,
+        verbose_name="Raven email address",
     )
     institution = models.ForeignKey(
         Institution,
         on_delete=models.CASCADE,
         help_text="Institution user is based",
+    )
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+    )
+    phone = models.CharField(
+        max_length=15,
+        blank=True,
     )
     AWAITING_APPROVAL = 1
     APPROVED = 2
