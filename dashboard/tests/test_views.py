457d2ab00b47f8c2a4490d8a9eec6a3edd6605fa
tysta kartografen <tysta.kartografen@gmail.com>
2018-04-13 19:35:10 +0100

    Dashboard unit tests.

--- dashboard/tests/test_views.py
+++ dashboard/tests/test_views.py
@@ -8,12 +8,14 @@ from users.tests.test_models import CustomUserTests
 class DashboardViewTests(TestCase):
 
     def setUp(self):
+        # Create an institution
         base_domain = 'bangor.ac.uk'
         InstitutionTests().create_institution(
             name='Bangor University',
             base_domain=base_domain,
         )
 
+        # Create a technical lead user account
         self.techlead_username = 'scw_techlead@' + base_domain
         self.techlead_password = '123456'
         self.techlead_user = CustomUserTests().create_techlead_user(
@@ -21,6 +23,7 @@ class DashboardViewTests(TestCase):
             password=self.techlead_password,
         )
 
+        # Create a student user account
         self.student_username = 'scw_student@' + base_domain
         self.student_password = '789123'
         self.student_user = CustomUserTests().create_student_user(
@@ -28,70 +31,75 @@ class DashboardViewTests(TestCase):
             password=self.student_password,
         )
 
-    def test_dashboard_access_as_an_unauthorised_shibboleth_user(self):
+    def test_dashboard_view_as_an_unauthorised_shibboleth_user(self):
         """
-        If the REMOTE_USER header is not present then the user should be
-        redirected to the login page.
+        If the REMOTE_USER header is not present then the user should be redirected to the login page.
         """
         headers = {
             'REMOTE_USER': None,
         }
-        response = self.client.get(reverse('home'), **headers)
+        response = self.client.get(
+            reverse('home'),
+            **headers,
+        )
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, '/accounts/login/?next=/')
 
-    def test_dashboard_access_as_an_authorised_shibboleth_user_and_unregistred_application_user(self):
+    def test_dashboard_view_as_an_authorised_shibboleth_user_and_unregistered_application_user(self):
         """
-        If the REMOTE_USER and eppn header is present and the user is not
-        a registered application user then the user should be redirected to
-        the account registration page.
+        If the REMOTE_USER and eppn header is present and the user is not a registered application 
+        user then the user should be redirected to the account registration page.
         """
         headers = {
             'REMOTE_USER': 'unregistered-application-user@bangor.ac.uk',
             'eppn': 'unregistered-application-user@bangor.ac.uk',
         }
-        response = self.client.get(reverse('home'), **headers)
+        response = self.client.get(
+            reverse('home'),
+            **headers,
+        )
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/accounts/register/')
+        self.assertEqual(response.url, reverse('register'))
 
-    def test_dashboard_access_as_an_authorised_techlead_user(self):
+    def test_dashboard_view_as_an_authorised_shibboleth_and_registered_technical_lead_user(self):
         """
-        If the REMOTE_USER and eppn header is present and the user is a
-        registered application user then the user should be redirected to
-        the dashboard page.
+        If the REMOTE_USER and eppn header is present and the user is a registered application user 
+        then the user should be redirected to the dashboard page and have the option to logout.
         """
         headers = {
             'REMOTE_USER': self.techlead_username,
             'eppn': self.techlead_username,
         }
-
         # Ensure the dashboard is available.
-        response = self.client.get(reverse('home'), **headers)
+        response = self.client.get(
+            reverse('home'),
+            **headers,
+        )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context_data.get('project_user_requests_count'), 0)
 
         # Logout the user.
         response = self.client.get(reverse('logout'))
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/accounts/logged_out/')
+        self.assertEqual(response.url, reverse('logged_out'))
 
-    def test_dashboard_access_as_an_authorised_student_user(self):
+    def test_dashboard_view_as_an_authorised_shibboleth_and_registered_student_user(self):
         """
-        If the REMOTE_USER and eppn header is present and the user is a
-        registered application user then the user should be redirected to
-        the dashboard page.
+        If the REMOTE_USER and eppn header is present and the user is a registered application user 
+        then the user should be redirected to the dashboard page and have the option to logout.
         """
         headers = {
             'REMOTE_USER': self.student_username,
             'eppn': self.student_username,
         }
-
         # Ensure the dashboard is available.
-        response = self.client.get(reverse('home'), **headers)
+        response = self.client.get(
+            reverse('home'),
+            **headers,
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertIsNone(response.context_data.get('project_user_requests_count'))
 
         # Logout the user.
         response = self.client.get(reverse('logout'))
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/accounts/logged_out/')
+        self.assertEqual(response.url, reverse('logged_out'))
--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,5 @@
 from . import views
+
 from django.urls import path
 
 urlpatterns = [
