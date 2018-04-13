186a429db0616ebeded58839c9726d06647cad22
tysta kartografen <tysta.kartografen@gmail.com>
2018-04-13 19:37:54 +0100

    Dashboard cleanup.

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,10 +14,10 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         if self.request.user.has_perm('project.change_projectusermembership'):
             # How many project user membership requests are awaiting authorisation?
             projects = Project.objects.filter(tech_lead=self.request.user)
-            project_user_memberships = ProjectUserMembership.objects.filter(
+            project_user_memberships_count = ProjectUserMembership.objects.filter(
                 project__in=projects,
                 status=ProjectUserMembership.AWAITING_AUTHORISATION,
-            )
-            context['project_user_requests_count'] = project_user_memberships.count()
+            ).count()
+            context['project_user_requests_count'] = project_user_memberships_count
 
         return context
