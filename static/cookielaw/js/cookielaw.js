e0993600c8d212b8187f0653b50a82012bdfd32c
tysta kartografen <tysta.kartografen@gmail.com>
2018-04-16 23:48:17 +0100

    Misc improvements detected by codacy.

--- static/cookielaw/js/cookielaw.js
+++ static/cookielaw/js/cookielaw.js
@@ -1,7 +1,7 @@
 var Cookielaw = {
     createCookie: function(name, value, days) {
         var date = new Date(),
-            expires = '';
+            expires = "";
         if (days) {
             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toGMTString();
@@ -11,11 +11,11 @@ var Cookielaw = {
         document.cookie = name + "=" + value + expires + "; path=/";
     },
     createCookielawCookie: function() {
-        this.createCookie("cookielaw_accepted", '1', 10 * 365);
+        this.createCookie("cookielaw_accepted", "1", 10 * 365);
         if (typeof(window.jQuery) === "function") {
             jQuery("#CookielawBanner").hide();
         } else {
-            document.getElementById("CookielawBanner").style.display = 'none';
+            document.getElementById("CookielawBanner").style.display = "none";
         }
     }
 };
