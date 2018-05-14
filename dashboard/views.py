5052621209f6fbe02159d115587e34b0e3680f15
tystakartografen <tysta.kartografen@gmail.com>
2018-05-14 21:16:33 +0100

    UI updates to dashboard.

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 from project.models import ProjectUserMembership
+from users.models import Profile
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
@@ -13,5 +14,10 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         if self.request.user.has_perm('project.change_projectusermembership'):
             num_requests = ProjectUserMembership.objects.awaiting_authorisation(self.request.user).count()
             context['project_user_requests_count'] = num_requests
+
+        if self.request.user.profile.account_status == Profile.AWAITING_APPROVAL:
+            context['account_status_message'] = (
+                'Your account is currently being configured. You should receive an email shortly '
+                'with instructions on how to access the compute cluster.')
 
         return context
