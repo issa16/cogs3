76f919f034ce0fc542313e3210b91a3cd103df7b
Mark Dawson <Mark.Dawson@swansea.ac.uk>
2018-05-16 10:25:20 +0100

    institution links identified by id on login page

--- institution/models.py
+++ institution/models.py
@@ -34,5 +34,8 @@ class Institution(models.Model):
     def __str__(self):
         return self.name
 
+    def id_str(self):
+        return self.name.lower().replace(" ", "-")
+
     class Meta:
         ordering = ('name', )
