beb147dc57e7b0bea134ecb74858d835d4e7ac61
j.m.o.rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2018-05-16 17:08:49 +0100

    Added checks to the project form test

--- project/tests/test_integration.py
+++ project/tests/test_integration.py
@@ -1,32 +1,27 @@
-from project.models import ProjectFundingSource
 from selenium_base import SeleniumTestsBase
-
+from project.models import Project
 
 class ProjectIntegrationTests(SeleniumTestsBase):
 
-    def fill_project_form(self):
-        form_fields = {
-            "id_title": "Test project",
-            "id_description": "This project aims to test the submission of projects",
-            "id_institution": "swansea",
-            "id_institution_reference": "test1",
-            "id_department": "SA2C",
-            "id_pi": "Joe Bloggs",
-            "id_start_date": "2018-09-17",
-            "id_end_date": "2019-09-17",
-            "id_requirements_software": "none",
-            "id_requirements_gateways": "none",
-            "id_requirements_training": "none",
-            "id_requirements_onboarding": "none",
-            "id_allocation_cputime": "200",
-            "id_allocation_memory": "1",
-            "id_allocation_storage_home": "200",
-            "id_allocation_storage_scratch": "1",
-        }
-        self.fill_form_by_id(form_fields)
+    default_project_form_fields = {
+        "id_title": "Test project",
+        "id_description": "This project aims to test the submission of projects",
+        "id_institution": "swansea",
+        "id_institution_reference": "test1",
+        "id_department": "SA2C",
+        "id_pi": "Joe Bloggs",
+        "id_start_date": "2018-09-17",
+        "id_end_date": "2019-09-17",
+        "id_requirements_software": "none",
+        "id_requirements_gateways": "none",
+        "id_requirements_training": "none",
+        "id_requirements_onboarding": "none",
+        "id_allocation_cputime": "200",
+        "id_allocation_memory": "1",
+        "id_allocation_storage_home": "200",
+        "id_allocation_storage_scratch": "1",
+    }
 
-        self.select_from_dropdown('id_funding_source', 1)
-        self.submit_form(form_fields)
 
     def test_create_project(self):
         """
@@ -36,10 +31,23 @@ class ProjectIntegrationTests(SeleniumTestsBase):
         self.sign_in()
         self.click_by_id("create-project-application-button")
 
-        self.fill_project_form()
+        self.fill_form_by_id(self.default_project_form_fields)
+        self.select_from_dropdown('id_funding_source', 1)
+
+        #Check that the project does not exist yet
+        matching_projects = Project.objects.filter(title=self.default_project_form_fields['id_title'])
+        print(matching_projects)
+        assert matching_projects.count() == 0
+
+        self.submit_form(self.default_project_form_fields)
 
         assert "This field is required." not in self.selenium.page_source
         assert "Successfully submitted a project application." in self.selenium.page_source
+
+        #Check that the project was created
+        matching_projects = Project.objects.filter(title=self.default_project_form_fields['id_title'])
+        print(matching_projects)
+        assert matching_projects.count() == 1
 
     def test_create_project_unauthorized(self):
         """
