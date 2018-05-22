6f778c4c07b9649c397a977ad3ee645aa8bd1755
tystakartografen <tysta.kartografen@gmail.com>
2018-05-22 21:14:49 +0100

    Formatting.

--- institution/models.py
+++ institution/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 from institution.exceptions import InvalidIndentityProvider
 from institution.exceptions import InvalidInstitution
-from django.utils.translation import gettext as _
 
 
 class Institution(models.Model):
