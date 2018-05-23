27288681ec2e3c8e8a22520e1342fe5dd3372776
j.m.o.rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2018-05-23 13:37:51 +0100

    Shibboleth user creation needs to be tested manually

--- users/tests/test_integration.py
+++ users/tests/test_integration.py
@@ -10,12 +10,3 @@ class UserIntegrationTests(SeleniumTestsBase):
         for user in [self.user, self.external, self.student, self.rse, self.admin,]:
             self.sign_in(user)
             self.log_out()
-
-
-    def test_create_user(self):
-        """
-        Create a new user with an institutional email address
-        """
-        self.get_url("/accounts/login/?entityID=https://iss-openathensla-runtime.swan.ac.uk/oala/metadata")
-        # No shibboleth login in the test case
-        assert False
