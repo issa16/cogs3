004f5857bc69a63e4c8e0063f22401606ad010e1
tystakartografen <tysta.kartografen@gmail.com>
2018-06-14 15:22:55 +0100

    Updated user import to read uid_number.

--- users/management/commands/add_shibboleth_users.py
+++ users/management/commands/add_shibboleth_users.py
@@ -8,7 +8,7 @@ from users.models import Profile
 
 
 class Command(BaseCommand):
-    help = 'Create or update shibboleth user profile data.'
+    help = 'Create or update Shibboleth users.'
 
     def add_arguments(self, parser):
         parser.add_argument('csv_filename')
@@ -19,42 +19,50 @@ class Command(BaseCommand):
             with open(filename, newline='') as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
-                    user, created = CustomUser.objects.get_or_create(
-                        username=row['institutional_address'],
-                        email=row['institutional_address'],
-                        first_name=row['firstname'],
-                        last_name=row['surname'],
-                        is_shibboleth_login_required=True,
-                    )
-                    if created:
-                        user.set_password(CustomUser.objects.make_random_password())
-                        user.save()
-                        self.stdout.write(
-                            self.style.SUCCESS('Successfully created user account: ' + row['institutional_address']))
-                    else:
-                        self.stdout.write(self.style.SUCCESS(row['institutional_address'] + ' already exists!'))
+                    try:
+                        user, created = CustomUser.objects.get_or_create(
+                            username=row['institutional_address'].lower(),
+                            email=row['institutional_address'].lower(),
+                            first_name=row['firstname'].title(),
+                            last_name=row['surname'].title(),
+                            is_shibboleth_login_required=True,
+                        )
+                        if created:
+                            user.set_password(CustomUser.objects.make_random_password())
+                            user.save()
+                            message = 'Successfully created user account: {email}'.format(
+                                email=row['institutional_address'])
+                            self.stdout.write(self.style.SUCCESS(message))
+                        else:
+                            message = '{email} already exists.'.format(email=row['institutional_address'])
+                            self.stdout.write(self.style.SUCCESS(message))
 
-                    # Update shibboleth user profile.
-                    profile = user.profile
-                    profile.scw_username = row['new_scw_username']
-                    profile.hpcw_username = row['hpcw_username']
-                    profile.hpcw_email = row['hpcw_email']
-                    profile.raven_username = row['raven_username']
-                    profile.raven_email = row['raven_email']
-                    profile.description = row['description']
-                    profile.phone = row['phone']
-                    profile.account_status = Profile.AWAITING_APPROVAL
-                    profile.shibboleth_id = row['institutional_address']
+                        profile = user.profile
+                        profile.scw_username = row['new_scw_username']
+                        profile.hpcw_username = row['hpcw_username'].title()
+                        profile.hpcw_email = row['hpcw_email'].lower()
+                        profile.raven_username = row['raven_username']
+                        if row['raven_uid']:
+                            profile.uid_number = int(row['raven_uid'])
+                        profile.raven_email = row['raven_email'].lower()
+                        profile.description = row['description']
+                        profile.phone = row['phone']
+                        profile.account_status = Profile.AWAITING_APPROVAL
+                        profile.shibboleth_id = row['institutional_address'].lower()
 
-                    # Pending new fields?
-                    # profile.department = row['department']?
-                    # profile.orcid = row['orcid']?
-                    # profile.scopus = row['scopus']?
-                    # profile.homepage = row['homepage']?
-                    # profile.cronfa = row['cronfa']?
+                        # Pending new fields?
+                        # profile.department = row['department']?
+                        # profile.orcid = row['orcid']?
+                        # profile.scopus = row['scopus']?
+                        # profile.homepage = row['homepage']?
+                        # profile.cronfa = row['cronfa']?
 
-                    profile.save()
-                    self.stdout.write(
-                        self.style.SUCCESS('Successfully updated user profile: ' + row['institutional_address']))
+                        profile.save()
+                        message = 'Successfully updated user profile: {email}'.format(
+                            email=row['institutional_address'])
+                        self.stdout.write(self.style.SUCCESS(message))
+                    except Exception as e:
+                        message = '{error}\n{row}'.format(error=e, row=row)
+                        self.stdout.write(self.style.ERROR(message))
         except FileNotFoundError:
             self.stdout.write(self.style.ERROR('Unable to open ' + filename))
