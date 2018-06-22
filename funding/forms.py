b90a6c9953f796b71d67b6d15a29868efcebe3ad
j.m.o.rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2018-06-22 13:25:56 +0100

    Set pi to user when available

--- funding/forms.py
+++ funding/forms.py
@@ -6,7 +6,16 @@ from institution.models import Institution
 class FundingSourceForm(forms.ModelForm):
     class Meta:
         model = FundingSource
-        fields = ['title', 'identifier', 'funding_body','pi_email']
+        fields = ['title', 'identifier', 'funding_body', 'pi_email']
+
+    def __init__(self, *args, **kwargs):
+        # Set the initial email if pi is a user
+        instance = kwargs.get('instance', {})
+        if instance.pi is not None:
+            initial = kwargs.get('initial', {})
+            initial['pi_email'] = instance.pi.email
+            kwargs['initial'] = initial
+        super(FundingSourceForm, self).__init__(*args, **kwargs)
 
     def clean_pi_email(self):
         cleaned_data = super().clean()
