752684af3bb569811660252e7a3460705ac97144
tystakartografen <tysta.kartografen@gmail.com>
2018-06-26 16:39:00 +0100

    institution unit tests.

--- institution/tests/test_models.py
+++ institution/tests/test_models.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
+from institution.exceptions import InvalidIndentityProvider
+from institution.exceptions import InvalidInstitution
 from institution.models import Institution
 
 
 class InstitutionTests(TestCase):
+
+    fixtures = [
+        'institution/fixtures/tests/mock_institutions.yaml',
+    ]
 
     @classmethod
     def create_institution(cls, name, base_domain, identity_provider):
@@ -25,9 +31,9 @@ class InstitutionTests(TestCase):
         """
         Ensure we can create an Institution instance.
         """
-        name = 'Example University'
-        base_domain = 'example.ac.uk'
-        identity_provider = 'https://idp.example.ac.uk/shibboleth'
+        name = 'Test University'
+        base_domain = 'test-example.ac.uk'
+        identity_provider = 'https://idp.test-example.ac.uk/shibboleth'
         institution = self.create_institution(
             name=name,
             base_domain=base_domain,
@@ -39,10 +45,20 @@ class InstitutionTests(TestCase):
         self.assertEqual(institution.base_domain, base_domain)
         self.assertEqual(institution.identity_provider, identity_provider)
 
+    def test_valid_email_address(self):
+        self.assertTrue(Institution.is_valid_email_address('user@example.ac.uk'))
+
+    def test_invalid_email_address(self):
+        with self.assertRaises(InvalidInstitution):
+            Institution.is_valid_email_address('invalid-email')
+
+    def test_valid_identity_provider(self):
+        self.assertTrue(Institution.is_valid_identity_provider('https://idp.example.ac.uk/shibboleth'))
+
+    def test_invalid_identity_provider(self):
+        with self.assertRaises(InvalidIndentityProvider):
+            Institution.is_valid_identity_provider('invalid-identity-provider')
+
     def test_id_str_produced(self):
-        institution = self.create_institution(
-            name='Example University',
-            base_domain='example.ac.uk',
-            identity_provider='https://idp.example.ac.uk/shibboleth',
-        )
+        institution = Institution.objects.get(name='Example University')
         self.assertEqual(institution.id_str(), "example-university")
