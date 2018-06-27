494d17b9b3a9412f79ffa027a9cdda8c8450800b
tystakartografen <tysta.kartografen@gmail.com>
2018-06-27 17:28:35 +0100

    Refactoring.

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -2,8 +2,21 @@ from django.test import TestCase
 from django.utils.translation import activate
 
 from institution.models import Institution
+from users.forms import CustomUserChangeForm
 from users.forms import CustomUserCreationForm
 from users.models import CustomUser
+
+
+class CustomUserChangeFormTests(TestCase):
+
+    fixtures = [
+        'institution/fixtures/tests/institutions.json',
+        'users/fixtures/tests/users.json',
+    ]
+
+    def setUp(self):
+        self.institution = Institution.objects.get(name='Example University')
+        self.user = CustomUser.objects.get(email='joe.bloggs@example.ac.uk')
 
 
 class CustomUserCreationFormTests(TestCase):
@@ -15,36 +28,28 @@ class CustomUserCreationFormTests(TestCase):
     def setUp(self):
         self.institution = Institution.objects.get(name='Example University')
 
-    def test_create_shibboleth_user(self):
+    def test_create_user(self):
         """
-        Ensure the user creation form works for a shibboleth user.
+        Ensure the user creation form works for institutional and external users.
         """
-        form = CustomUserCreationForm(
-            data={
-                'email': '@'.join(['joe.bloggs', self.institution.base_domain]),
-                'first_name': 'Joe',
-                'last_name': 'Bloggs',
-                'is_shibboleth_login_required': True,
-            }, )
-        self.assertTrue(form.is_valid())
-
-    def test_create_non_shibboleth_user(self):
-        """
-        Ensure the user creation form works for a non shibboleth user.
-        """
-        form = CustomUserCreationForm(
-            data={
-                'email': '@'.join(['joe.bloggs', self.institution.base_domain]),
-                'first_name': 'Joe',
-                'last_name': 'Bloggs',
-                'is_shibboleth_login_required': False,
-            }, )
-        self.assertTrue(form.is_valid())
+        test_cases = {
+            '@'.join(['joe.bloggs', self.institution.base_domain]): True,
+            'joe.bloggs@external.ac.uk': False,
+        }
+        for email, shibboleth_required in test_cases.items():
+            form = CustomUserCreationForm(
+                data={
+                    'email': email,
+                    'first_name': 'Joe',
+                    'last_name': 'Bloggs',
+                    'is_shibboleth_login_required': shibboleth_required,
+                }, )
+            self.assertTrue(form.is_valid())
 
     def test_invalid_institution_email(self):
         """
         Ensure an email address from an unsupported institution domain is caught via the
-        CustomUserCreationForm if the user is required to login via a shibboleth IDP.
+        CustomUserCreationForm, if the user is required to login via a shibboleth IDP.
         """
         form = CustomUserCreationForm(
             data={
@@ -59,26 +64,30 @@ class CustomUserCreationFormTests(TestCase):
         """
         Ensure a CustomUser instance can not be created without the required form fields.
         """
-        form = CustomUserCreationForm(data={})
         activate('en')
+        form = CustomUserCreationForm(data={})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['email'], ['This field is required.'])
         self.assertEqual(form.errors['first_name'], ['This field is required.'])
         self.assertEqual(form.errors['last_name'], ['This field is required.'])
 
-    def test_password_generation_for_shibboleth_user(self):
+    def test_password_generation(self):
         """
-        Ensure a random password is genereted for a shibboleth user.
+        Ensure a random password is genereted new user accounts.
         """
-        email = '@'.join(['joe.bloggs', self.institution.base_domain])
-        form = CustomUserCreationForm(
-            data={
-                'email': email,
-                'first_name': 'Joe',
-                'last_name': 'Bloggs',
-                'is_shibboleth_login_required': True,
-            }, )
-        self.assertTrue(form.is_valid())
-        form.save()
-        self.assertEqual(CustomUser.objects.filter(email=email).count(), 1)
-        self.assertIsNotNone(CustomUser.objects.get(email=email).password)
+        test_cases = {
+            '@'.join(['joe.bloggs', self.institution.base_domain]): True,
+            'joe.bloggs@external.ac.uk': False,
+        }
+        for email, shibboleth_required in test_cases.items():
+            form = CustomUserCreationForm(
+                data={
+                    'email': email,
+                    'first_name': 'Joe',
+                    'last_name': 'Bloggs',
+                    'is_shibboleth_login_required': shibboleth_required,
+                }, )
+            self.assertTrue(form.is_valid())
+            form.save()
+            self.assertEqual(CustomUser.objects.filter(email=email).count(), 1)
+            self.assertIsNotNone(CustomUser.objects.get(email=email).password)
