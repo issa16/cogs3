7e4e4cf9faf701c016510ffd57977c25e10da0df
j.m.o.rantaharju <j.m.o.rantaharju@swansea.ac.uk>
2018-09-19 15:35:55 +0100

    Check for active allocations before disabling project

--- project/openldap.py
+++ project/openldap.py
@@ -22,7 +22,12 @@ def update_openldap_project(allocation):
         else:
             project_api.create_project.delay(project=project)
     elif project.status in deactivate_project_states:
-        project_api.deactivate_project.delay(project=project)
+        # Check for other approved allocations before deactivating
+        if not SystemAllocationRequest.objects.filter(
+            project=project,
+            status=SystemAllocationRequest.APPROVED
+        ).exists:
+            project_api.deactivate_project.delay(project=project)
 
 
 def update_openldap_project_membership(project_membership):
