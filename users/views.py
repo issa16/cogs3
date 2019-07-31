issa16/cogs3

commit 30d63e890370700b04c9e8e8791f6659f1984810

move towards #16, but lots of tests fail
 - many shibboleth components missing when running test suite
 - no tests written to test new functionality added yet
 - functionality hasn't been tested by hand due to lack of ability to test logging in as a new shibbolethuser

users/views.py
@@ -4,11 +4,38 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views import generic
 
+from common.util import mailing_list_subscribe
 from users.forms import RegisterForm
 from users.forms import TermsOfServiceForm
 from users.models import CustomUser
+from institution.models import Institution
+
+class MailingListMixin:
+    def dispatch(self, request, *args, **kwargs):
+        if request.session.shib.username:
+            _, domain = request.session.shib.username.split('@')
+            self.institution = Institution.objects.get(base_domain=domain)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['mailing_list'] = None
+        context['institution_name'] = None
+        if self.institution:
+            context['mailing_list'] = institution.local_mailing_list_name
+            context['institution_name'] = _(institution.name)
+
+        return context
+
+    def form_valid(self, form):
+        if self.institution and form.instance.join_mailing_list:
+            mailing_list_subscribe(
+                self.institution.local_mailing_list_link,
+                form.instance.email
+            )
+        return super().form_valid(form)
 
 
 class TermsOfService(LoginRequiredMixin, generic.UpdateView):
@@ -21,7 +48,8 @@ class TermsOfService(LoginRequiredMixin, generic.UpdateView):
         return self.request.user
 
 
-class CompleteRegistrationView(LoginRequiredMixin, generic.UpdateView):
+class CompleteRegistrationView(LoginRequiredMixin, MailingListMixin,
+                               generic.UpdateView):
     form_class = RegisterForm
     model = CustomUser
     success_url = reverse_lazy('login')
@@ -40,7 +68,7 @@ class CompleteRegistrationView(LoginRequiredMixin, generic.UpdateView):
         return super().dispatch(*args, **kwargs)
 
 
-class RegisterView(generic.CreateView):
+class RegisterView(MailingListMixin, generic.CreateView):
     form_class = RegisterForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'

common/util.py
@@ -2,6 +2,12 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 
+import logging
+import requests
+
+class SubscriptionFailedError(Exception):
+    pass
+
 
 def email_user(subject, context, text_template_path, html_template_path):
     """
@@ -26,3 +32,27 @@ def email_user(subject, context, text_template_path, html_template_path):
     )
     email.attach_alternative(html_alternative, "text/html")
     email.send(fail_silently=False)
+
+
+def mailing_list_subscribe(url, email):
+    """
+    Subscribe a user to a mailing list.
+
+    Args:
+        url: A URL that will subscribe a user to a mailing list, when the
+             variable 'email' is replaced with an email address.
+
+        email: The user's email address.
+    """
+
+    try:
+        request = requests.get(url.format(email=email))
+        if request.status_code < 200 or request.status_code >= 400:
+            raise SubscriptionFailedError(
+                "Server returned HTTP {}".format(request.status_code)
+            )
+    except Exception as ex:
+        logger = logging.getLogger('apps')
+        logger.exception(
+            "Unable to subscribe %s to mailing list: %s", email, str(ex)
+        )

institution/migrations/0015_auto_20180821_1530.py
@@ -1,0 +1,23 @@
+# Generated by Django 2.0.2 on 2018-08-21 15:30
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('institution', '0014_institution_rse_notify_email'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='institution',
+            name='local_mailing_list_link',
+            field=models.CharField(max_length=255, null=True),
+        ),
+        migrations.AddField(
+            model_name='institution',
+            name='local_mailing_list_name',
+            field=models.CharField(max_length=100, null=True),
+        ),
+    ]

institution/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from institution.exceptions import InvalidInstitutionalEmailAddress
 from institution.exceptions import InvalidInstitutionalIndentityProvider
@@ -26,6 +26,8 @@ class Institution(models.Model):
     local_repository_name = models.CharField(max_length=100, blank=True)
     local_repository_domain = models.CharField(max_length=100, blank=True)
     funding_database_name = models.CharField(max_length=100, blank=True)
+    local_mailing_list_name = models.CharField(max_length=100, null=True)
+    local_mailing_list_link = models.CharField(max_length=255, null=True)
     logo_path = models.CharField(max_length=255, blank=True)
 
     @classmethod

templates/registration/register.html
@@ -27,6 +27,16 @@
 				<label for="reason-for-account">{% trans "Reason for account" %}</label>
 				<textarea rows="5" class="form-control" id="reason-for-account" name="reason_for_account" required></textarea>
 			</div>
+			{% if mailing_list %}
+			<div class="form-group mb-3">
+				<div class="form-check">
+					<input class="form-check-input" type="checkbox" id="join-mailing-list" name="join_mailing_list">
+					<label for="join-mailing-list">{% blocktrans %}
+					Sign me up to the {{ mailing_list }} mailing list to keep up to date with HPC-related activities at {{ institution_name }}.
+					{% endblocktrans %}</label>
+				</div>
+			</div>
+			{% endif %}
 			<div class="form-group text-center mb-3">
 				<div class="form-check">
 					<input class="form-check-input" type="checkbox" id="accepted-terms-and-conditions" name="accepted_terms_and_conditions" required>

users/forms.py
@@ -96,6 +96,7 @@ class RegisterForm(forms.ModelForm):
             'last_name',
             'reason_for_account',
             'accepted_terms_and_conditions',
+            'join_mailing_list',
         )
 
     def __init__(self, *args, **kwargs):

users/migrations/0011_customuser_join_mailing_list.py
@@ -1,0 +1,18 @@
+# Generated by Django 2.0.2 on 2018-08-21 15:30
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0010_auto_20180820_1551'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='customuser',
+            name='join_mailing_list',
+            field=models.BooleanField(default=False),
+        ),
+    ]

users/models.py
@@ -298,6 +298,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         default=False,
         verbose_name=_('Terms and Conditions'),
     )
+    join_mailing_list = models.BooleanField(
+        default=False,
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []

commit 11cc8bad12c2016a281274f8067e6a0a230df2d5

bugfix in accessing shibboleth session object in mailing list mixin
Bug introduced in commit 30d63e890370700b04c9e8e8791f6659f1984810
(initial solution issue #16)

It isn't clear to me how much this implementation is tested.

users/views.py
@@ -15,8 +15,8 @@ from institution.models import Institution
 
 class MailingListMixin:
     def dispatch(self, request, *args, **kwargs):
-        if request.session.shib.username:
-            _, domain = request.session.shib.username.split('@')
+        if request.session['shib']['username']:
+            _, domain = request.session['shib']['username'].split('@')
             self.institution = Institution.objects.get(base_domain=domain)
 
     def get_context_data(self, **kwargs):