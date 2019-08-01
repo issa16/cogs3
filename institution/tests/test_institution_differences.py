73919f77db6ad9b60bb6e039c2d493883b1c5961
Michele Mesiti <michele.mesiti@swansea.ac.uk>
2019-08-01 17:45:28 +0100

    Still writing tests for difference between institutions.
    Some business logic must be understood. The website behaves strangely
    now.

--- institution/tests/test_institution_differences.py
+++ institution/tests/test_institution_differences.py
@@ -1,6 +1,9 @@
 ''' 
 This tests are written to check the behavioural differences 
-between different institutions.
+between different institutions, from the user point of view.
+This test focuses only on the differences in the features available,
+not on the features themselves, that is: these tests do not check that 
+a feature works.
 
 The general idea is to test 
 
@@ -10,7 +13,7 @@ Conditions:
 Outcomes:
     a. User can/cannot see a certain feature on the web pages 
 
-This is an end-to-end test that checks also the database.
+This is an end-to-end test that checks also the fixtures. 
 '''
 
 from selenium_base import SeleniumTestsBase
@@ -21,6 +24,8 @@ from unittest import skipIf  # DEBUG
 from time import sleep  # DEBUG
 
 from django.urls import reverse
+from django.test import tag
+from selenium.common.exceptions import NoSuchElementException
 import re
 
 
@@ -36,47 +41,106 @@ class InstitutionDifferencesIntegrationTest(SeleniumTestsBase):
         # There are an Aberystwyth user, a Bangor user, a Cardiff user
         # and a Swansea user
 
-        # For conveninence and for DRY code
-        def _createuser(instname):
-            institution = Institution.objects.get(name=f"{instname} University")
-            # tid : Test Institution Differences
-            email = '@'.join(['user_tid', institution.base_domain])
-            return CustomUser(
-                username=email,
-                email=email,
-                first_name=instname,
-                last_name='User',
-                is_staff=False,
-                is_shibboleth_login_required=True,
-                accepted_terms_and_conditions=True,
-            )
-
+        # USERS WITH AUTHORISATION
         # Create user for Aberystwyth
-        self.aberystwyth_user = _createuser(instname='Aberystwyth')
+        self.aberystwyth_user = self._createuser(instname='Aberystwyth')
         self.create_test_user(self.aberystwyth_user)
 
         # Create user for Bangor
-        self.bangor_user = _createuser(instname='Bangor')
+        self.bangor_user = self._createuser(instname='Bangor')
         self.create_test_user(self.bangor_user)
 
         # Create user for Cardiff
-        self.cardiff_user = _createuser(instname='Cardiff')
+        self.cardiff_user = self._createuser(instname='Cardiff')
         self.create_test_user(self.cardiff_user)
 
         # Create user for Swansea
-        self.swansea_user = _createuser(instname='Swansea')
+        self.swansea_user = self._createuser(instname='Swansea')
         self.create_test_user(self.swansea_user)
-        self.project_title = 'My Fake Project'
 
-    def _create_fake_project(self, separate_allocation_requests):
+        # USERS AWAITING AUTHORISATION
+        # Create user for Aberystwyth
+        self.aberystwyth_user_aa = self._createuser(instname='Aberystwyth',
+                username = 'user_tid_aa')
+        self._create_test_user_unapproved(self.aberystwyth_user)
 
+        # Create user for Bangor
+        self.bangor_user_aa = self._createuser(instname='Bangor',
+                username = 'user_tid_aa')
+        self._create_test_user_unapproved(self.bangor_user)
+
+        # Create user for Cardiff
+        self.cardiff_user_aa = self._createuser(instname='Cardiff',
+                username = 'user_tid_aa')
+        self._create_test_user_unapproved(self.cardiff_user)
+
+        # Create user for Swansea
+        self.swansea_user_aa = self._createuser(instname='Swansea',
+                username = 'user_tid_aa')
+        self._create_test_user_unapproved(self.swansea_user)
+
+        self.fake_project_title = 'My Fake Project'
+
+    def _create_test_user_unapproved(self, user):
+        user.set_password(self.user_password)
+        domain = user.email.split('@')[1]
+        user.save()
+
+    # For conveninence and for DRY code
+    def _createuser(self,instname,username = 'user_tid'):
+        institution = Institution.objects.get(name=f"{instname} University")
+        # tid : Test Institution Differences
+        email = '@'.join([username, institution.base_domain])
+        return CustomUser(
+            username=email,
+            email=email,
+            first_name=instname,
+            last_name='User',
+            is_staff=False,
+            is_shibboleth_login_required=True,
+            accepted_terms_and_conditions=True,
+        )
+
+
+    def _go_to_dashboard(self):
+        self.selenium.find_element_by_link_text('Dashboard').click()
+
+    def _go_to_project_creation_from_dashboard(self):
+        # We need to find the link to click first
         link_text = re.search(
             r'<a href=.*>(.*Create.*Project.*)</a>', self.selenium.page_source
         ).groups()[0]
         self.selenium.find_element_by_link_text(link_text).click()
 
+    def _go_to_project_list_from_dashboard(self):
+        link_text = re.search(
+            r'<a href=.*>(\s*Projects\s*)</a>', self.selenium.page_source
+        ).groups()[0]
+        self.selenium.find_element_by_link_text(link_text).click()
+
+    def _select_or_create_fake_project_in_project_list(
+        self, separate_allocation_requests
+    ):
+        try:
+            el = self.selenium.find_element_by_link_text(
+                self.fake_project_title
+            )
+        except NoSuchElementException:
+            self._create_fake_project(separate_allocation_requests)
+            self._go_to_dashboard()
+            self._go_to_project_list_from_dashboard()
+            el = self.selenium.find_element_by_link_text(
+                self.fake_project_title
+            )
+        el.click()
+
+    def _create_fake_project(self, separate_allocation_requests):
+
+        self._go_to_dashboard()
+        self._go_to_project_creation_from_dashboard()
+
         fields = {
-            'id_title': self.project_title,
+            'id_title': self.fake_project_title,
             'id_description': 'fake project description',
             'id_supervisor_email': 'pi@aber.ac.uk'
         }
@@ -90,16 +154,13 @@ class InstitutionDifferencesIntegrationTest(SeleniumTestsBase):
         self.fill_form_by_id(fields)
         self.submit_form(fields)
 
+    # Allocation/project separation
     def _check_allocation_separation(self, user, separate_allocation_requests):
         # user signs in
         self.sign_in(user)
-        # user creates new project
-        # We need to find the link to click first
-        link_text = re.search(
-            r'<a href=.*>(.*Create.*Project.*)</a>', self.selenium.page_source
-        ).groups()[0]
-        self.selenium.find_element_by_link_text(link_text).click()
+        self._go_to_project_creation_from_dashboard()
 
+        # user creates new project
         if separate_allocation_requests:
             # There should be no allocation section here:
             if "Allocation" in self.selenium.page_source:
@@ -112,7 +173,7 @@ class InstitutionDifferencesIntegrationTest(SeleniumTestsBase):
         # TODO: we fill the project form using ids
         # TODO: would it be better to use instead labels regexps?
         fields = {
-            'id_title': self.project_title,
+            'id_title': self.fake_project_title,
             'id_description': 'fake project description',
             'id_supervisor_email': 'pi@aber.ac.uk'
         }
@@ -131,16 +192,10 @@ class InstitutionDifferencesIntegrationTest(SeleniumTestsBase):
         if "allocation requests" in self.selenium.page_source and not separate_allocation_requests:
             raise AssertionError('"allocation request" string in page source.')
 
-        self.selenium.find_element_by_link_text('Dashboard').click()
-
-        # checking projects
-        link_text = re.search(
-            r'<a href=.*>(\s*Projects\s*)</a>', self.selenium.page_source
-        ).groups()[0]
-        self.selenium.find_element_by_link_text(link_text).click()
-
+        self._go_to_dashboard()
+        self._go_to_project_list_from_dashboard()
         # selecting project
-        self.selenium.find_element_by_link_text(self.project_title).click()
+        self.selenium.find_element_by_link_text(self.fake_project_title).click()
 
         if separate_allocation_requests:
             # There should be an allocation request section here:
@@ -157,131 +212,240 @@ class InstitutionDifferencesIntegrationTest(SeleniumTestsBase):
 
         self.log_out()
 
-    # Allocation/project separation
-    #@skipIf(True,'I want to skip this')
+    @tag("proj_alloc_sep")
     def test_aberystwyth_user_sees_allocation_separation_NO(self):
         self._check_allocation_separation(
             user=self.aberystwyth_user, separate_allocation_requests=False
         )
 
-    #@skipIf(True,'I want to skip this')
+    @tag("proj_alloc_sep")
     def test_bangor_user_sees_allocation_separation_NO(self):
         self._check_allocation_separation(
             user=self.bangor_user, separate_allocation_requests=False
         )
 
-    #@skipIf(True,'I want to skip this')
+    @tag("proj_alloc_sep")
     def test_cardiff_user_sees_allocation_separation_NO(self):
         self._check_allocation_separation(
             user=self.cardiff_user, separate_allocation_requests=False
         )
 
-    #@skipIf(True,'I want to skip this')
+    @tag("proj_alloc_sep")
     def test_swansea_user_sees_allocation_separation_YES(self):
         self._check_allocation_separation(
             user=self.swansea_user, separate_allocation_requests=True
         )
 
-
-
-    def _check_funding_workflow(self,user,need_funding_workflow):
+    def _check_funding_workflow(
+        self, user, need_funding_workflow, separate_allocation_requests
+    ):
         self.sign_in(user)
+
+        # check the dashboard
         attribution_link_search_res = re.search(
-                r'<a href=.*>(\s*Attributions\s*)</a>', self.selenium.page_source
-            )
+            r'<a href=.*>(\s*Attributions\s*)</a>', self.selenium.page_source
+        )
         if need_funding_workflow:
-            # There is an Attribution link 
+            # There is an Attribution link
             link_text = attribution_link_search_res.groups()[0]
         else:
+            # there should be no mentions of Attributions
             assert attribution_link_search_res is None, 'Attribution link is on the page'
 
-        # TODO: check the rest
- 
-    # funding workflow
-    def test_aberystwyth_user_sees_funding_workflow_YES(self):
-        self._check_funding_workflow(self.aberystwyth_user,need_funding_workflow = True)
+        # check the project creation form
+        self._go_to_project_creation_from_dashboard()
 
-    #@skipIf(True,'I want to skip this')
+        match = re.search('[fF]unding\s+[sS]ources', self.selenium.page_source)
+        if need_funding_workflow:
+            # There is mention of Funding sources and grants
+            assert match is not None, 'No mention of funding sources in page source'
+        else:
+            assert match is None, 'Funding sources should not be mentioned in page source'
+
+        self._go_to_dashboard()
+        self._go_to_project_list_from_dashboard()
+        # selecting project
+        self._select_or_create_fake_project_in_project_list(
+            separate_allocation_requests
+        )
+        # checking that there is/ there is not 'Attributions' in the page source.
+        match = re.search('[Aa]ttributions', self.selenium.page_source)
+        if need_funding_workflow:
+            # There is mention of Attributions
+            assert match is not None, 'No mention of Attributions in page source'
+            # There is a 'manage attributions' link
+            try:
+                el = self.selenium.find_element_by_link_text(
+                    "Manage attributions"
+                )
+            except NoSuchElementException:
+                raise AssertionError(
+                    'A "Manage Attributions" link should be present'
+                )
+        else:
+            assert match is None, 'Attributions should not be mentioned in page source'
+
+    @tag("fund_wf")
+    def test_aberystwyth_user_sees_funding_workflow_YES(self):
+        self._check_funding_workflow(
+            self.aberystwyth_user,
+            need_funding_workflow=True,
+            separate_allocation_requests=False
+        )
+
+    @tag("fund_wf")
     def test_bangor_user_sees_funding_workflow_NO(self):
-        self._check_funding_workflow(self.bangor_user,need_funding_workflow = False)
- 
-    #@skipIf(True,'I want to skip this')
+        self._check_funding_workflow(
+            self.bangor_user,
+            need_funding_workflow=False,
+            separate_allocation_requests=False
+        )
+
+    @tag("fund_wf")
     def test_cardiff_user_sees_funding_workflow_NO(self):
-        self._check_funding_workflow(self.cardiff_user,need_funding_workflow = False)
- 
-    #@skipIf(True,'I want to skip this')
+        self._check_funding_workflow(
+            self.cardiff_user,
+            need_funding_workflow=False,
+            separate_allocation_requests=False
+        )
+
+    @tag("fund_wf")
     def test_swansea_user_sees_funding_workflow_YES(self):
-        self._check_funding_workflow(self.swansea_user,need_funding_workflow = True)
- 
-#    # RSE requests
-#    @skipIf(True,'I want to skip this')
-#    def test_aberystwyth_user_sees_rse_requests_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_bangor_user_sees_rse_requests_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_cardiff_user_sees_rse_requests_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_swansea_user_sees_rse_requests_YES(self):
-#        pass
-#
-#
-#    # funding approval
-#    @skipIf(True,'I want to skip this')
-#    def test_aberystwyth_user_sees_funding_approval_YES(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_bangor_user_sees_funding_approval_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_cardiff_user_sees_funding_approval_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_swansea_user_sees_funding_approval_YES(self):
-#        pass
-#
-#    # supervisor approval
-#    @skipIf(True,'I want to skip this')
-#    def test_aberystwyth_user_sees_supervisor_approval_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_bangor_user_sees_supervisor_approval_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_cardiff_user_sees_supervisor_approval_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_swansea_user_sees_supervisor_approval_YES(self):
-#        pass
-#
-#    # user approval
-#    @skipIf(True,'I want to skip this')
-#    def test_aberystwyth_user_sees_user_approval_NO(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_bangor_user_sees_user_approval_YES(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_cardiff_user_sees_user_approval_YES(self):
-#        pass
-#
-#    @skipIf(True,'I want to skip this')
-#    def test_swansea_user_sees_user_approval_NO(self):
-#        pass
-#
-#
-#
-#
+        self._check_funding_workflow(
+            self.swansea_user,
+            need_funding_workflow=True,
+            separate_allocation_requests=True
+        )
+
+    # RSE requests
+    def _check_rse_request_availability(
+        self, user, allows_rse_requests, separate_allocation_requests
+    ):
+        self.sign_in(user)
+        self._go_to_project_list_from_dashboard()
+        self._select_or_create_fake_project_in_project_list(
+            separate_allocation_requests
+        )
+
+        try:
+            # TODO: use regexp instead?
+            strtofind = "New RSE time Request"
+            el = self.selenium.find_element_by_link_text(strtofind)
+            if not allows_rse_requests:
+                raise AssertionError(
+                    f'A "{strtofind}" link should not be present'
+                )
+        except NoSuchElementException:
+            if allows_rse_requests:
+                raise AssertionError(f'A "{strtofind}" link should be present')
+
+    @tag("RSE_reqs")
+    def test_aberystwyth_user_sees_rse_requests_NO(self):
+        self._check_rse_request_availability(
+            self.aberystwyth_user,
+            allows_rse_requests=False,
+            separate_allocation_requests=False
+        )
+
+    @tag("RSE_reqs")
+    def test_bangor_user_sees_rse_requests_NO(self):
+        self._check_rse_request_availability(
+            self.bangor_user,
+            allows_rse_requests=False,
+            separate_allocation_requests=False
+        )
+
+    @tag("RSE_reqs")
+    def test_cardiff_user_sees_rse_requests_NO(self):
+        self._check_rse_request_availability(
+            self.cardiff_user,
+            allows_rse_requests=False,
+            separate_allocation_requests=False
+        )
+
+    @tag("RSE_reqs")
+    def test_swansea_user_sees_rse_requests_YES(self):
+        self._check_rse_request_availability(
+            self.swansea_user,
+            allows_rse_requests=True,
+            separate_allocation_requests=True
+        )
+
+    # funding approval
+    def _create_funding_source(self):
+        pass # TODO, Workflow must be understood first
+
+
+
+    def _check_funding_approval_availability(
+        self, user, needs_funding_approval, separate_allocation_requests
+    ):
+        self.sign_in(user)
+
+    @skipIf(True, 'Workflow must be understood first')
+    def test_aberystwyth_user_sees_funding_approval_YES(self):
+        pass
+
+    @skipIf(True, 'Workflow must be understood first')
+    @skipIf(
+        True, 'Bangor users do not need funding workflow in the first place.'
+    )
+    def test_bangor_user_sees_funding_approval_NO(self):
+        pass
+
+    @skipIf(True, 'Workflow must be understood first')
+    @skipIf(
+        True, 'Cardiff users do not need funding workflow in the first place'
+    )
+    def test_cardiff_user_sees_funding_approval_NO(self):
+        pass
+
+    @skipIf(True, 'Workflow must be understood first')
+    def test_swansea_user_sees_funding_approval_YES(self):
+        pass
+
+
+    # supervisor approval
+    @skipIf(True,'Not implemented yet')
+    def test_aberystwyth_user_sees_supervisor_approval_NO(self):
+        pass
+
+    @skipIf(True,'Not implemented yet')
+    def test_bangor_user_sees_supervisor_approval_NO(self):
+        pass
+
+    @skipIf(True,'Not implemented yet')
+    def test_cardiff_user_sees_supervisor_approval_NO(self):
+        pass
+
+    @skipIf(True,'Not implemented yet')
+    def test_swansea_user_sees_supervisor_approval_YES(self):
+        pass
+
+    # user approval
+    def _check_user_approval_matters(self,user,needs_user_approval):
+        # at the dashboard
+        self.sign_in(user)
+        # check that we see what 
+        pass # TODO, Workflow must be understood first
+
+
+    @skipIf(True,'Workflow must be understood first')
+    def test_aberystwyth_user_sees_user_approval_NO(self):
+        pass
+
+    @skipIf(True,'Workflow must be understood first')
+    def test_bangor_user_sees_user_approval_YES(self):
+        pass
+
+    @skipIf(True,'Workflow must be understood first')
+    def test_cardiff_user_sees_user_approval_YES(self):
+        pass
+
+    @skipIf(True,'Workflow must be understood first')
+    def test_swansea_user_sees_user_approval_NO(self):
+        pass
+
+
+
+
