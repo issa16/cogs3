e546950969082dff34d8e069b11ecbfde088cef1
tystakartografen <tysta.kartografen@gmail.com>
2019-10-23 16:03:27 +0100

    Added LDAP and email tests for ProfileUpdateForm

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -1,5 +1,7 @@
 import datetime
+import pprint
 
+import mock
 from django import forms
 from django.conf import settings
 from django.core import mail
@@ -7,11 +9,13 @@ from django.test import TestCase, override_settings
 from django.utils.translation import activate
 
 from institution.models import Institution
+from openldap.tests.test_api import OpenLDAPBaseAPITests
 from users.forms import (
     CustomUserChangeForm, CustomUserCreationForm, ProfileUpdateForm,
     RegisterForm
 )
 from users.models import CustomUser, Profile
+from users.tests.test_models import CustomUserTests
 
 
 class ProfileUpdateFormTests(TestCase):
@@ -30,41 +34,152 @@ class ProfileUpdateFormTests(TestCase):
             email='guest.user@external.ac.uk'
         )
 
-    def test_profile_update(self):
+    @mock.patch('requests.post')
+    def test_profile_activation(self, post_mock):
         """
-        Ensure the profile update form works for institutional and external users.
+        Ensure the profile update form works for institutional and external
+        users when activating a user's account.
         """
-        test_cases = [
-            self.shibboleth_user,
-            self.guest_user,
-        ]
-        for test_case in test_cases:
-            scw_username = 'x.test.username'
+        mail.outbox = []  # Clear mail outbox
+
+        user = CustomUserTests.create_custom_user(
+            email='activation-test@example.ac.uk',
+            is_shibboleth_login_required=True
+        )
+        description = 'Test User'
+        account_status = Profile.APPROVED
+        form = ProfileUpdateForm(
+            data={
+                'user': user.pk,
+                'scw_username': None,
+                'description': description,
+                'account_status': account_status,
+            },
+            instance=user.profile,
+        )
+        self.assertTrue(form.is_valid())
+
+        # Mock the LDAP API post request
+        jwt = (
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL29wZ'
+            'W5sZGFwLmV4YW1wbGUuY29tLyIsImF1ZCI6Imh0dHBzOi8vb3BlbmxkYXAuZX'
+            'hhbXBsZS5jb20vIiwiaWF0IjoxNTI3MTAwMTQxLCJuYmYiOjE1MjcwOTk1NDE'
+            'sImRhdGEiOnsiY24iOiJlLmpvZS5ibG9nZ3MiLCJzbiI6IkJsb2dncyIsImdp'
+            'ZE51bWJlciI6IjUwMDAwMDEiLCJnaXZlbm5hbWUiOiJKb2UiLCJkaXNwbGF5T'
+            'mFtZSI6Ik1yIEpvZSBCbG9nZ3MiLCJ0aXRsZSI6Ik1yIiwiaG9tZWRpcmVjdG'
+            '9yeSI6Ii9ob21lL2Uuam9lLmJsb2dncyIsImxvZ2luc2hlbGwiOiIvYmluL2J'
+            'hc2giLCJvYmplY3RjbGFzcyI6WyJpbmV0T3JnUGVyc29uIiwicG9zaXhBY2Nv'
+            'dW50IiwidG9wIl0sInRlbGVwaG9uZW51bWJlciI6IjAwMDAwLTAwMC0wMDAiL'
+            'CJtYWlsIjoiYWN0aXZhdGlvbi10ZXN0QGV4YW1wbGUuYWMudWsiLCJ1aWQiOi'
+            'JlLmpvZS5ibG9nZ3MiLCJ1aWRudW1iZXIiOiI1MDAwMDAxIn19.d_KDASWxtR'
+            'z6rBbgaHWqpR-XvUtdl22BB9QNYUz2-Ko'
+        )
+        post_mock.return_value = OpenLDAPBaseAPITests._mock_response(
+            self, status=201, content=jwt.encode()
+        )
+        form.save()  # Trigger LDAP API call
+
+        self.assertEqual(user.profile.scw_username, 'e.joe.bloggs')
+        self.assertEqual(user.profile.uid_number, '5000001')
+        self.assertEqual(user.profile.description, description)
+        self.assertEqual(user.profile.account_status, account_status)
+
+        # Ensure account status change was propagated to LDAP
+        call_args, call_kwargs = post_mock.call_args_list[0]
+        call_url = call_args[0]
+        expected_call_url = f'{settings.OPENLDAP_HOST}user/'
+        self.assertEqual(call_url, expected_call_url)
+        expected_call_kwargs = {
+            'data': {
+                'department': '',
+                'email': 'activation-test@example.ac.uk',
+                'firstName': 'Joe',
+                'surname': 'Bloggs',
+            },
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cache-Control': 'no-cache'
+            },
+            'timeout': 5
+        }
+        self.assertEqual(call_kwargs, expected_call_kwargs)
+
+        # Ensure the user was notified of the change in account status
+        self.assertEqual(len(mail.outbox), 1)
+        email = mail.outbox[0]
+        self.assertEqual(email.to, [user.email])
+        self.assertNotEqual(email.subject.find('Account Created'), -1)
+        self.assertNotEqual(email.body.find('User Account Update'), -1)
+        self.assertNotEqual(email.body.find(user.first_name), -1)
+
+        # Clear call list
+        post_mock.call_args_list = []
+
+    @mock.patch('requests.delete')
+    def test_profile_deactivation(self, delete_mock):
+        """
+        Ensure the profile update form works for institutional and external
+        users when deactivating a user's account.
+        """
+        users = [self.shibboleth_user, self.guest_user]
+        for user in users:
+            mail.outbox = []  # Clear mail outbox
+
+            scw_username = 'scw_username'
             uid_number = 5000001
-            description = 'test user'
-            account_status = 1
+            description = 'Test User'
+            account_status = Profile.SUSPENDED
             form = ProfileUpdateForm(
                 data={
-                    'user': test_case.pk,
+                    'user': user.pk,
                     'scw_username': scw_username,
                     'uid_number': uid_number,
                     'description': description,
                     'account_status': account_status,
                 },
-                instance=test_case.profile,
+                instance=user.profile,
             )
             self.assertTrue(form.is_valid())
-            form.save()
 
-            self.assertEqual(test_case.profile.scw_username, scw_username)
-            self.assertEqual(test_case.profile.uid_number, uid_number)
-            self.assertEqual(test_case.profile.description, description)
-            self.assertEqual(test_case.profile.account_status, account_status)
+            # Mock the LDAP API delete request
+            delete_mock.return_value = OpenLDAPBaseAPITests._mock_response(
+                self, status=204
+            )
+            form.save()  # Trigger the LDAP API call
+
+            self.assertEqual(user.profile.scw_username, scw_username)
+            self.assertEqual(user.profile.uid_number, uid_number)
+            self.assertEqual(user.profile.description, description)
+            self.assertEqual(user.profile.account_status, account_status)
+
+            # Ensure account status change was propagated to LDAP
+            call_args, call_kwargs = delete_mock.call_args_list[0]
+            call_url = call_args[0]
+            expected_call_url = f'{settings.OPENLDAP_HOST}user/{user.email}/'
+            self.assertEqual(call_url, expected_call_url)
+            expected_call_kwargs = {
+                'headers': {
+                    'Cache-Control': 'no-cache'
+                },
+                'timeout': 5
+            }
+            self.assertEqual(call_kwargs, expected_call_kwargs)
+
+            # Ensure the user was notified of the change in account status
+            self.assertEqual(len(mail.outbox), 1)
+            email = mail.outbox[0]
+            self.assertEqual(email.to, [user.email])
+            self.assertNotEqual(email.subject.find('Account Deactivated'), -1)
+            self.assertNotEqual(email.body.find('User Account Update'), -1)
+            self.assertNotEqual(email.body.find(user.first_name), -1)
+
+            # Clear call list
+            delete_mock.call_args_list = []
 
     def test_pre_approved_options(self):
         """
-        Ensure the correct account status options are available for accounts that are awaiting 
-        approval.
+        Ensure the correct account status options are available for accounts 
+        that are awaiting approval.
         """
         self.shibboleth_user.profile.account_status = Profile.AWAITING_APPROVAL
         self.shibboleth_user.profile.save()
@@ -81,6 +196,7 @@ class ProfileUpdateFormTests(TestCase):
             instance=self.shibboleth_user.profile,
         )
         self.assertTrue(form.is_valid())
+        form.save()
 
         expected_choices = Profile.PRE_APPROVED_OPTIONS
         actual_choices = form.fields['account_status'].widget.choices
@@ -88,8 +204,8 @@ class ProfileUpdateFormTests(TestCase):
 
     def test_post_approved_options(self):
         """
-        Ensure the correct account status options are available for accounts that have been 
-        approved.
+        Ensure the correct account status options are available for accounts 
+        that have been approved.
         """
         self.shibboleth_user.profile.account_status = Profile.APPROVED
         self.shibboleth_user.profile.save()
@@ -105,16 +221,14 @@ class ProfileUpdateFormTests(TestCase):
             instance=self.shibboleth_user.profile,
         )
         self.assertTrue(form.is_valid())
+        form.save()
 
         expected_choices = Profile.POST_APPROVED_OPTIONS
         actual_choices = form.fields['account_status'].widget.choices
         self.assertEqual(actual_choices, expected_choices)
 
 
-@override_settings(
-    EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend',
-    DEFAULT_SUPPORT_EMAIL='admin_team@example.ac.uk'
-)
+@override_settings(DEFAULT_SUPPORT_EMAIL='admin_team@example.ac.uk')
 class CustomUserCreationFormTests(TestCase):
 
     fixtures = [
@@ -128,11 +242,11 @@ class CustomUserCreationFormTests(TestCase):
         """
         Ensure the user creation form works for institutional and external users.
         """
-        test_cases = {
+        users = {
             '@'.join(['shibboleth.user', self.institution.base_domain]): True,
             'guest.user@external.ac.uk': False,
         }
-        for email, shibboleth_required in test_cases.items():
+        for email, shibboleth_required in users.items():
             data = {
                 'email': email,
                 'first_name': 'Joe',
@@ -183,11 +297,11 @@ class CustomUserCreationFormTests(TestCase):
         """
         Ensure a random password is genereted new user accounts.
         """
-        test_cases = {
+        users = {
             '@'.join(['shibboleth.user', self.institution.base_domain]): True,
             'guest.user@external.ac.uk': False,
         }
-        for email, shibboleth_required in test_cases.items():
+        for email, shibboleth_required in users.items():
             form = CustomUserCreationForm(
                 data={
                     'email': email,
