002d06f4d2375af74acec08fcf460e759124f1b7
unifi-dev <info@unifidev.com>
2020-11-19 15:41:03 +0000

    Placeholders for data import from CSV

---
+++ stats/management/commands/import_daily_stats.py
@@ -0,0 +1,13 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Import Daily Stats from csv file.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_filename')
+
+    def handle(self, *args, **options):
+        pass
