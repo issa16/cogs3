5005b74828313f03b24a463d842ab058f3839b5a
unifi-dev <info@unifidev.com>
2020-11-19 15:32:54 +0000

    Fixed reference to Cluster field

--- system/models.py
+++ system/models.py
@@ -28,6 +28,23 @@ class OperatingSystem(models.Model):
         return f'{self.name}'
 
 
+class Cluster(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.CharField(max_length=512)
+    total_number_of_cores = models.IntegerField()
+    system = models.ForeignKey(
+        System,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Queue(models.Model):
     """
     Represents a system queue.
@@ -35,7 +52,7 @@ class Queue(models.Model):
     name = models.CharField(max_length=32, unique=True)
     description = models.CharField(max_length=512)
     cluster = models.ForeignKey(
-        System,
+        Cluster,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
@@ -47,23 +64,6 @@ class Queue(models.Model):
         null=True,
     )
     cores_per_node = models.IntegerField()
-    created_time = models.DateTimeField(auto_now_add=True)
-    modified_time = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.name}'
-
-
-class Cluster(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    description = models.CharField(max_length=512)
-    total_number_of_cores = models.IntegerField()
-    system = models.ForeignKey(
-        System,
-        on_delete=models.SET_NULL,
-        blank=True,
-        null=True,
-    )
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
 
