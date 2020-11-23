59f31c358d106c4b6fb264ab43144ab7d48fabb5
unifi-dev <info@unifidev.com>
2020-11-23 20:54:56 +0000

    Added placeholder stats pages

--- stats/urls.py
+++ stats/urls.py
@@ -1,10 +1,26 @@
-from stats.views import StatsView
+from . import views
+
 from django.urls import path
 
 urlpatterns = [
     path(
         'data-analytics/',
-        StatsView.as_view(),
+        views.StatsView.as_view(),
         name='data-analytics',
+    ),
+    path(
+        'data-analytics/project-data/',
+        views.ProjectDataView.as_view(),
+        name='project-data',
+    ),
+    path(
+        'data-analytics/compute-consumption-data/',
+        views.ComputeConsumptionDataView.as_view(),
+        name='compute-consumption-data',
+    ),
+    path(
+        'data-analytics/storage-consumption-data/',
+        views.StorageConsumptionDataView.as_view(),
+        name='storage-consumption-data',
     ),
 ]
--- stats/views.py
+++ stats/views.py
@@ -4,3 +4,15 @@ from django.views.generic import TemplateView
 
 class StatsView(LoginRequiredMixin, TemplateView):
     template_name = 'stats/index.html'
+
+
+class ProjectDataView(LoginRequiredMixin, TemplateView):
+    template_name = 'stats/project_data.html'
+
+
+class ComputeConsumptionDataView(LoginRequiredMixin, TemplateView):
+    template_name = 'stats/compute_consumption_data.html'
+
+
+class StorageConsumptionDataView(LoginRequiredMixin, TemplateView):
+    template_name = 'stats/storage_consumption_data.html'
