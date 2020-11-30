6fdf6ca8d1939e706e9c3e7bcb5bd8de99d4e056
unifi-dev <info@unifidev.com>
2020-11-30 15:18:02 +0000

    (Phase 2) Visualisation

--- stats/urls.py
+++ stats/urls.py
@@ -8,19 +8,4 @@ urlpatterns = [
         views.StatsView.as_view(),
         name='data-analytics',
     ),
-    path(
-        'data-analytics/project-data/',
-        views.ProjectDataView.as_view(),
-        name='project-data',
-    ),
-    path(
-        'data-analytics/compute-consumption-data/',
-        views.ComputeConsumptionDataView.as_view(),
-        name='compute-consumption-data',
-    ),
-    path(
-        'data-analytics/storage-consumption-data/',
-        views.StorageConsumptionDataView.as_view(),
-        name='storage-consumption-data',
-    ),
 ]
