5ad5739c96b81201c71974aac0b0e193085d109d
unifi-dev <73905599+unifi-dev@users.noreply.github.com>
2021-03-16 13:06:25 +0000

    Update import_historical_weekly_storage.py

--- stats/management/commands/import_historical_weekly_storage.py
+++ stats/management/commands/import_historical_weekly_storage.py
@@ -1,26 +1,22 @@
 import os
-import shutil
 
 from django.core.management.base import BaseCommand
 
+# Usage: python3 manage.py import_weekly_storage {input_dir}
 
 class Command(BaseCommand):
     help = 'Import historical weekly storage stats from csv files.'
 
     def add_arguments(self, parser):
         parser.add_argument('--input_dir', required=True, help='Path to csv files to import', type=str)
-        parser.add_argument('--output_dir', required=True, help='Path to move csv files to once processed', type=str)
 
     def handle(self, *args, **options):
         try:
             input_dir = options['input_dir']
-            output_dir = options['output_dir']
 
             # Validate paths
             if os.path.exists(input_dir) is False:
                 raise Exception(f'{input_dir} does not exist.')
-            if os.path.exists(output_dir) is False:
-                raise Exception(f'{output_dir} does not exist.')
 
             # Process csv files
             for filename in os.listdir(input_dir):
@@ -51,20 +47,9 @@ class Command(BaseCommand):
                             -s {code}"
                     )
 
-                    # Once complete, move home and scratch files from
-                    # input_dir to output_dir.
-                    shutil.move(
-                        home_file,
-                        os.path.join(output_dir, filename),
-                    )
-                    shutil.move(
-                        scratch_file,
-                        os.path.join(output_dir, filename),
-                    )
-
                     self.stdout.write(self.style.SUCCESS(f'Finished processing {os.path.join(input_dir, filename)}'))
 
         except Exception as e:
-            self.stdout.write(self.style.ERROR(e))
+            self.stderr.write(self.style.ERROR(e))
 
         self.stdout.write(self.style.SUCCESS('Finished processing csv files.'))
