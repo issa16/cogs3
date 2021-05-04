94b76b4254ad5a49d51b9dd1390fca2e75f84bdf
Tysta Kartografen <tystakartografen@users.noreply.github.com>
2021-05-04 17:46:07 +0100

    Update migrate_pi_text_field.py

--- project/management/commands/migrate_pi_text_field.py
+++ project/management/commands/migrate_pi_text_field.py
@@ -8,14 +8,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for project in Project.objects.all():
             try:
-                if project.status == Project.APPROVED:
-                    name, position, email = project.pi.split(',')
-                    project.pi_name = name.title().strip()
-                    project.pi_position = position.title().strip()
-                    project.pi_email = email.lower().strip()
-                    project.save()
-                    msg = f'{project.code} Updated.'
-                    self.stdout.write(self.style.SUCCESS(msg))
+                name, position, email = project.pi.split(',')
+                project.pi_name = name.title().strip()
+                project.pi_position = position.title().strip()
+                project.pi_email = email.lower().strip()
+                project.save()
+                msg = f'{project.code} Updated.'
+                self.stdout.write(self.style.SUCCESS(msg))
             except Exception as e:
                 msg = f'{project.code} {e}'
                 self.stdout.write(self.style.ERROR(msg))
